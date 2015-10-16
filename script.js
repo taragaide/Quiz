c37703229a8e2d2bc4ccffa8e2c2ea7aba480ca1
taragaide <taragaide@yahoo.com>
2015-10-15 22:13:55 -0400

    jQuery to JS, 1st attempt

--- script.js
+++ script.js
@@ -1,144 +1,85 @@
-$(document).ready(function() {
-   var correctAnswers = 0;
-   //quiz array
-   var quiz = [ {
-question: "What year was the Monopoly board game invented?",
-      choices: ["1932", "1946", "1931", "1935"],
-      correct: 4
-   },
+$(document).ready(function(){
 
+var curPage=0;
+var myAnswers = [];
+var myQuiz=[
+  ["What year was the Monopoly board game invented?", "1933", "1946", "1931", "1935", 1],
+
+  ["What is the name of the space that you start the game from?","Free Parking", "Chance", "Just Visiting", "Go!", 4],
+
+  ["The banker MAY be a player?", "True", "Who Cares", "Unsure", "False", 1],
+
+  ["You can go to jail by three methods. Which one is NOT a way to go to jail?", "Landing on the jail square by roll of dice", "Moved by chance or community chest card", "Opponent physically placing your piece in jail", "Rolling doubles three times in one turn", 3]
+
+
+];
+
+var myHeader = document.getElementById("quizHeader");
+var classname = document.getElementsByClassName("answer");
+var myQuestion = document.getElementById("questions");
+var progressBar=document.getElementById("progressBar");
+var btnNext=document.getElementById("btnNext");
+var btnPrevious=document.getElementById("btnPrevious");
+var btnFinish=document.getElementById("finishQuiz");
+checkPage();
+
+btnNext.addEventListener("click", moveNext);
+btnPrevious.addEventListener("click", moveBack);
+btnFinish.addEventListener("click", endQuiz);
+for (var i=0; i<classname.length; i++) {
+  classname[i].addEventListener('click', myAnswer, false);
+}
+
+
+function myAnswer (){
+    var idAnswer=this.getAttribute("data-id");
+    myAnswer[curPage]=idAnswer;
+    if (myQuiz[curPage][1]==idAnswer){
+      console.log("Correct Answer");
+    }
+    else
     {
-      question: "What is the name of the space that you start the game from?",
-      choices: ["Free Parking", "Chance", "Just Visiting", "Go!"],
-      correct: 4
-   },
-   {
-      question: "The banker MAY be a player",
-      choices: ["True", "Who Cares", "Unsure", "False" ],
-      correct: 1
-   },
-   {
-      question: "You can go to jail by three methods. Which one is NOT a way to go to jail?",
-      choices: ["Landing on the jail square by roll of dice", "Moved by chance or community chest card", "Opponent physically placing your piece in jail", "Rolling doubles three times in one turn"],
-      correct: 3
-   }];
+      console.log("Wrong Answer");
+    }
+    console.log(myAnswers);
+  }
 
 
-
-
-
-   var correctAnswers = 0;
-   
-
-   // functions
-   $(".app .start").on("click",function(){
-      var correctChoice = quiz[0].correct;
-      
-      $(".question").show();
-      $(".app .question").text(quiz[0].question);
-      $(".title").hide();
-      $(".form").show();
-      $(".app .answer-a").text(quiz[0].choices[0]);
-      $(".app .answer-b").text(quiz[0].choices[1]);         
-      $(".app .answer-c").text(quiz[0].choices[2]);
-      $(".app .answer-d").text(quiz[0].choices[3]);
-      $(".app .next.question-1").show();
-      $(".answers .button:eq(" + correctChoice + ")").addClass('correct');
-
-   });
-   $(".app .next.question-1").on("click",function(){
-      var correctChoice = quiz[1].correct;
-      
-      $(".answers .button:eq(3)").removeClass('correct');
-      $(".app .question").text(quiz[1].question);
-      $(".app .answer-a").text(quiz[1].choices[0]);
-      $(".app .answer-b").text(quiz[1].choices[1]);         
-      $(".app .answer-c").text(quiz[1].choices[2]);
-      $(".app .answer-d").text(quiz[1].choices[3]);
-      $(".button").css('background-color', '#010F4D');
-      $(this).hide();
-      $(".app .next.question-2").show();
-      $(".answers .button:eq(" + correctChoice + ")").addClass('correct');
-   });
-$(".app .next.question-2").on("click",function(){
-      var correctChoice = quiz[2].correct;
-      
-      $(".answers .button:eq(3)").removeClass('correct');
-      $(".app .question").text(quiz[2].question);
-      $(".app .answer-a").text(quiz[2].choices[0]);
-      $(".app .answer-b").text(quiz[2].choices[1]);         
-      $(".app .answer-c").text(quiz[2].choices[2]);
-      $(".app .answer-d").text(quiz[2].choices[3]);
-      $(".button").css('background-color', '#010F4D');
-      $(this).hide();
-      $(".app .next.question-3").show();
-      $(".answers .button:eq(" + correctChoice + ")").addClass('correct');
-   });
-
-
-
-   $(".app .next.question-3").on("click",function(){
-      var correctChoice = quiz[3].correct;
-      
-      $(".answers .button:eq(1)").removeClass('correct');
-      $(".app .question").text(quiz[3].question);
-      $(".app .answer-a").text(quiz[3].choices[0]);
-      $(".app .answer-b").text(quiz[3].choices[1]);         
-      $(".app .answer-c").text(quiz[3].choices[2]);
-      $(".app .answer-d").text(quiz[3].choices[3]);
-      $(".button").css('background-color', '#010F4D');
-      $(this).hide();
-      $(".app .next.finish").show();
-      
-      $(".answers .button:eq(" + correctChoice + ")").addClass('correct');
-   });
-
- 
-   $(".app .next.finish").on("click", function(){
-      var finalScore = Math.floor((correctAnswers/quiz.length) * 100);
-      $(".app .question").css('font-size', '45px');
-      if (finalScore === 100) {
-         $(".app .question").text("Congratulations! You got " + finalScore + " %");
-      } else if (finalScore === 75) {
-        $(".app .question").text("Good job! You got " + finalScore + " %");
-      } else if (finalScore === 50) {
-         $(".app .question").text("Alright! You got " + finalScore + " %");
-      } else {
-        $(".app .question").text("Oh no! You got " + finalScore + " %");
+function addBox(){
+  for (var i = 0; i<myQuestion.children.length; i++){
+      var curNode=myQuestion.children[i];
+      if (myAnswers[curPage]==(i+1)){
+        curNode.classList.add("seleAnswer");
       }
+      else {
+        curNode.classList.remove("seleAnswer");
+      }
+  }
 
-      $(".app .answer-a").hide();
-      $(".app .answer-b").hide();        
-      $(".app .answer-c").hide();
-      $(".app .answer-d").hide();
-      $(".button").hide();
-      $("button").hide();
-   });
+}
 
-//Select
-   $(".button-a").on("click", function(){
-          $(this).css('background-color', '#FE6281');
-   });
-   $(".button-b").on("click", function(){
-          $(this).css('background-color', '#FE6281');
-   });
-   $(".button-c").on("click", function(){
-          $(this).css('background-color', '#FE6281');
-   });
-   $(".button-d").on("click", function(){
-          $(this).css('background-color', '#FE6281');
-   });
-//Right or Wrong
-   $(".answers .button").on("click",function(){
-      var isCorrect = $(this).hasClass("correct");
-      
-       if (isCorrect) {
-          correctAnswers++;
-          console.log("Correct");
-       } else {
-          console.log("Wrong");
-       }
-   });
+
+function moveNext(){
+  if (myAnswers[curPage]){
+    console.log("please proceed");
+    if (curPage<(myQuiz.length-1)){
+      curPage++;
+      checkPage(curPage);
+    } else
+  }
+}
+
+
+function checkPage (i) {
+if (curPage==0){
+  btnPrevious.classList.add("hide");
+} 
+else {
+  btnP
+}
+
+}
+
 
 });
 
@@ -151,16 +92,4 @@ $(".app .next.question-2").on("click",function(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
