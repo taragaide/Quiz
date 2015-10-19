6bbcade387b0f91cfe8c30ac10e4b9b3ec11c826
taragaide <taragaide@yahoo.com>
2015-10-19 15:42:45 -0400

    jQuery to Javascript2

--- script.js
+++ script.js
@@ -1,95 +1,116 @@
-$(document).ready(function(){
 
-var curPage=0;
-var myAnswers = [];
-var myQuiz=[
-  ["What year was the Monopoly board game invented?", "1933", "1946", "1931", "1935", 1],
+$(function() {
 
-  ["What is the name of the space that you start the game from?","Free Parking", "Chance", "Just Visiting", "Go!", 4],
+            var curentPage = 0;
+            var myAnswers = [];
+            var myQuiz = [
+                ["What year was the Monopoly board game invented?", "1933", "1946", "1931", "1935", 1],
 
-  ["The banker MAY be a player?", "True", "Who Cares", "Unsure", "False", 1],
+                ["What is the name of the space that you start the game from?", "Free Parking", "Chance", "Just Visiting", "Go!", 4],
 
-  ["You can go to jail by three methods. Which one is NOT a way to go to jail?", "Landing on the jail square by roll of dice", "Moved by chance or community chest card", "Opponent physically placing your piece in jail", "Rolling doubles three times in one turn", 3]
+                ["The banker MAY be a player?", "True", "Who Cares", "Unsure", "False", 1],
 
-
-];
-
-var myHeader = document.getElementById("quizHeader");
-var classname = document.getElementsByClassName("answer");
-var myQuestion = document.getElementById("questions");
-var progressBar=document.getElementById("progressBar");
-var btnNext=document.getElementById("btnNext");
-var btnPrevious=document.getElementById("btnPrevious");
-var btnFinish=document.getElementById("finishQuiz");
-checkPage();
-
-btnNext.addEventListener("click", moveNext);
-btnPrevious.addEventListener("click", moveBack);
-btnFinish.addEventListener("click", endQuiz);
-for (var i=0; i<classname.length; i++) {
-  classname[i].addEventListener('click', myAnswer, false);
-}
+                ["You can go to jail by three methods. Which one is NOT a way to go to jail?", "Landing on the jail square by roll of dice", "Moved by chance or community chest card", "Opponent physically placing your piece in jail", "Rolling doubles three times in one turn", 3]
 
 
-function myAnswer (){
-    var idAnswer=this.getAttribute("data-id");
-    myAnswer[curPage]=idAnswer;
-    if (myQuiz[curPage][1]==idAnswer){
-      console.log("Correct Answer");
-    }
-    else
-    {
-      console.log("Wrong Answer");
-    }
-    console.log(myAnswers);
-  }
+            ];
+
+        });
+
+            document.getElementById("btnNext").addEventListener("click", moveNext);
+            document.getElementById("btnPrevious").addEventListener("click", moveBack);
+
+            var myOne = document.getElementById("page1");
+            var myTwo = document.getElementById("page2");
 
 
-function addBox(){
-  for (var i = 0; i<myQuestion.children.length; i++){
-      var curNode=myQuestion.children[i];
-      if (myAnswers[curPage]==(i+1)){
-        curNode.classList.add("seleAnswer");
-      }
-      else {
-        curNode.classList.remove("seleAnswer");
-      }
-  }
 
-}
+            //var curentPage=1;
+            var myHeader = document.getElementById("quizHeader");
+            var classname = document.getElementsByClassName("answer");
+            var myQuestion = document.getElementById("questions");
+            var progressBar = document.getElementById("progressBar");
+            var btnNext = document.getElementById("btnNext");
+            var btnPrevious = document.getElementById("btnPrevious");
+            var btnFinish = document.getElementById("finishQuiz");
 
-
-function moveNext(){
-  if (myAnswers[curPage]){
-    console.log("please proceed");
-    if (curPage<(myQuiz.length-1)){
-      curPage++;
-      checkPage(curPage);
-    } else
-  }
-}
+            //btnFinish.addEventListener("click", endQuiz);
+            //for (var i = 0; i < classname.length; i++) {
+             //   classname[i].addEventListener('click', myAnswer, false);
+            //}
 
 
-function checkPage (i) {
-if (curPage==0){
-  btnPrevious.classList.add("hide");
-} 
-else {
-  btnP
-}
+            console.log(classname);
 
-}
-
-
-});
+            console.log(myQuestion);
 
 
 
 
+            function myAnswer() {
+               var idAnswer = this.getAttribute("data-id");
+                myAnswer[currentPage] = idAnswer;
+               if (myQuiz[currentPage][1] == idAnswer) {
+                   console.log("Correct Answer");
+             } else {
+                  console.log("Wrong Answer");
+               }
+               console.log(myAnswers);
+            }
 
 
+            function addBox() {
+               for (var i = 0; i < myQuestion.children.length; i++) {
+                    var curNode = myQuestion.children[i];
+                    if (myAnswers[currentPage] == (i + 1)) {
+                         curNode.classList.add("seleAnswer");
+                     } else {
+                        curNode.classList.remove("seleAnswer");
+                     }
+               }
+
+             }
+
+             function moveNext() {
+                 if (myAnswers[currentPage]) {
+                   console.log("please proceed");
+                    if (currentPage < (myQuiz.length - 1)) {
+                        curPage++;
+                        checkPage(currentPage);
+                    }
+                }
+            }
 
 
+            function checkPage(i) {
+                console.log(currentPage);
+                if (currentPage == 1) {
+                    myOne.classList.add("show");
+                    myOne.classList.remove("hide");
+                    myTwo.classList.add("hide");
+                    myTwo.classList("show");
+                }
 
+                if (currentPage == 2) {
+                    myOne.classList.add("hide");
+                    myOne.classList.remove("show");
+                    myTwo.classList.add("show");
+                    myTwo.classList("hide");
+                }
+                myHeader.innerHTML=myQuiz[currentPage][0];
+                for (var i = 0; i < myQuestion.children.length; i++) {
+                 var curNode=myQuestion.children[i];
+                    console.log(curNode.childNodes[1].innerHTML);
+                  curNode.childNodes[1].innerHTML=myQuiz[currentPage][(i+1)];
+                 }
+                }
 
- 
+                function moveBack() {
+                    if (currentPage > 0) {
+                       currentPage--;
+                      checkPage(currentPage);
+                  } else {
+                       console.log("end of quiz page");
+                   }
+                
+                
